fceceda467b4328665ec19df2f7e240e34c44fb7
1000JI <chjh1992@gmail.com>
2020-11-09 18:45:44 +0900

    feat: Operator Practice -> just, from, map, filter etc... and subscribe, observeOn, subscribeOn, side-effect

--- Example/season1/step2/RxSwiftIn4Hours/ViewController.swift
+++ Example/season1/step2/RxSwiftIn4Hours/ViewController.swift
@@ -20,11 +20,47 @@ class ViewController: UITableViewController {
     }
 
     @IBAction func exJust1() {
-        Observable.just("Hello World")
-            .subscribe(onNext: { str in // str == "Hello World"
-                print(str) // "Hello World"
-            })
+//        Observable.just("Hello World")
+//            .subscribe(onNext: { str in // str == "Hello World"
+//                print(str) // "Hello World"
+//            })
+//            .disposed(by: disposeBag)
+        
+        // subscribe: 최종적으로 그 데이터를 받아서 사용 할 때
+//        Observable.from(["RxSwift", "In", "4", "Hours"])
+//            .single() // 하나만 들어와야 함
+//            .subscribe { event in
+//                switch event {
+//                case .next(let str): // 데이터 전달
+//                    print("next: \(str)")
+//                case .error(let err): // 에러 발생
+//                    print("error: \(err.localizedDescription)")
+//                case .completed:
+//                    print("completed")
+//                }
+//            }
+//            .disposed(by: disposeBag)
+        
+//        Observable.from(["RxSwift", "In", "4", "Hours"])
+////            .single()
+//            .subscribe { str in
+//                print(str)
+//            } onError: { error in
+//                print(error.localizedDescription)
+//            } onCompleted: {
+//                print("completed")
+//            } onDisposed: {
+//                print("disposed")
+//            }
+//            .disposed(by: disposeBag)
+        
+        Observable.from(["RxSwift", "In", "4", "Hours"])
+            .subscribe(onNext: output(_:))
             .disposed(by: disposeBag)
+    }
+    
+    func output(_ str: Any) -> Void {
+        print(str)
     }
 
     @IBAction func exJust2() {
@@ -86,16 +122,38 @@ class ViewController: UITableViewController {
     }
 
     @IBAction func exMap3() {
+        // Main Thread -> Concurrency Thread
+        // observeOn을 건 다음줄 부터 영향을 받게 됨
+//        Observable.just("800x600")
+//            .observeOn(ConcurrentDispatchQueueScheduler.init(qos: .default)) // Concurrent Thread
+//            .map { $0.replacingOccurrences(of: "x", with: "/") } // "800/600"
+//            .map { "https://picsum.photos/\($0)/?random" } // "https://picsum.photos/800/600/?random"
+//            .map { URL(string: $0) } // URL
+//            .filter { $0 != nil } // nil인지 Check
+//            .map { $0! } // 강제 언래핑
+//            .map { try Data(contentsOf: $0) }
+//            .map { UIImage(data: $0) }
+//            .observeOn(MainScheduler.instance) // image를 setting 하는 곳이기에 Main Thread
+//            .subscribe(onNext: { image in
+//                self.imageView.image = image
+//            })
+//            .disposed(by: disposeBag)
+        
         Observable.just("800x600")
-            .map { $0.replacingOccurrences(of: "x", with: "/") } // "800/600"
-            .map { "https://picsum.photos/\($0)/?random" } // "https://picsum.photos/800/600/?random"
-            .map { URL(string: $0) } // URL
-            .filter { $0 != nil } // nil인지 Check
-            .map { $0! } // 강제 언래핑
+            .map { $0.replacingOccurrences(of: "x", with: "/") }
+            .map { "https://picsum.photos/\($0)/?random" }
+            .map { URL(string: $0) }
+            .filter { $0 != nil }
+            .map { $0! }
             .map { try Data(contentsOf: $0) }
+            .subscribeOn(ConcurrentDispatchQueueScheduler.init(qos: .default))
             .map { UIImage(data: $0) }
+            .observeOn(MainScheduler.instance)
+            .do(onNext: { image in
+                print(image?.size)
+            })
             .subscribe(onNext: { image in
-                self.imageView.image = image
+                self.imageView.image = image // side-effect
             })
             .disposed(by: disposeBag)
     }
