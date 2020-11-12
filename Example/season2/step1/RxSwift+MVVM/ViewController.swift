9a99e2dd27a48ca4374c63f2e6dc62bf92796cda
1000JI <chjh1992@gmail.com>
2020-11-12 11:44:12 +0900

    feat: disposable, disposebag usage

--- Example/season2/step1/RxSwift+MVVM/ViewController.swift
+++ Example/season2/step1/RxSwift+MVVM/ViewController.swift
@@ -28,12 +28,19 @@ let MEMBER_LIST_URL = "https://my.api.mockaroo.com/members_with_avatar.json?key=
 class ViewController: UIViewController {
     @IBOutlet var timerLabel: UILabel!
     @IBOutlet var editView: UITextView!
+    
+    var disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
             self?.timerLabel.text = "\(Date().timeIntervalSince1970)"
         }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        disposeBag = DisposeBag()
     }
 
     private func setVisibleWithAnimation(_ v: UIView?, _ s: Bool) {
@@ -45,15 +52,28 @@ class ViewController: UIViewController {
         })
     }
     
-    func downloadJson(_ url: String) -> Observable<String?> {
-        return Observable.just("Hello World") // 1: "Hello World"
-//        return Observable.from(["Hello", "World"]) // 1: "Hello", 2: "World"
-        
-//        return Observable.create() { emitter in
-//            emitter.onNext("Hello World")
-//            emitter.onCompleted()
-//            return Disposables.create()
-//        }
+    func downloadJson(_ url: String) -> Observable<String> {
+        return Observable.create { emitter in
+            let url = URL(string: url)!
+            let task = URLSession.shared.dataTask(with: url) { (data, _, err) in
+                guard err == nil else {
+                    emitter.onError(err!)
+                    return
+                }
+                
+                if let dat = data, let json = String(data: dat, encoding: .utf8) {
+                    emitter.onNext(json)
+                }
+                
+                emitter.onCompleted()
+            }
+            
+            task.resume()
+            
+            return Disposables.create {
+                task.cancel()
+            }
+        }
     }
 
     // MARK: SYNC
@@ -65,20 +85,33 @@ class ViewController: UIViewController {
         setVisibleWithAnimation(activityIndicator, true)
         
         // 2. Observable로 오는 데이터를 받아서 처리하는 방법
-        _ = downloadJson(MEMBER_LIST_URL)
-            .subscribeOn(ConcurrentDispatchQueueScheduler(qos: .default))
-            .map { $0?.uppercased() }
+        let jsonObservable = downloadJson(MEMBER_LIST_URL)
+        let helloObservable = Observable.just("Hello World")
+        
+        Observable.zip(jsonObservable, helloObservable) { $1 + "\n" + $0 }
             .observeOn(MainScheduler.instance) // == DispatchQueue.main.async { }, Sugar API: Operator
             .subscribe(onNext: { json in
                 self.editView.text = json
                 self.setVisibleWithAnimation(self.activityIndicator, false)
             })
+            .disposed(by: disposeBag)
     }
 }
 
 /*
- * Operators
- - observeOn => 다음 줄 부터 영향을 줌.
- - subscribeOn => 첫째 줄에 영향을 줌, 위치에 상관 없음.
- - Scan => 누적하면서 다음꺼를 더해서 출력함.
+ Combining Observables
+ - CombindLatest
+    - http://reactivex.io/documentation/operators/combinelatest.html
+    - Zip과 비슷한데, Zip 같은 경우 하나씩 쌍을 이루어 내려 보낸다고 했다면,
+        CombindLatest는 한 쪽에 데이터가 없어도 마지막으로 있었던 데이터와 쌍을 이루어 내려보낸다.
+    - (1, 2, 3) & (A, B) => Zip => (1A, 2B)
+    - (1, 2, 3) & (A, B) => CombindLatest => (1A, 2B, 3B)
+ - Merge
+    - http://reactivex.io/documentation/operators/merge.html
+    - 두 개의 Observable을 하나의 Observable로 합치는 작업(단순)
+ - Zip
+    - http://reactivex.io/documentation/operators/zip.html
+    - 하나씩 생성되면 쌍으로 만들어서 내려보내는 작업
+    - 한쪽만 Observable이 있으면 다른 쪽에도 만들어져야 진행 할 수 있다.
+    - 데이터 형식이 달라도 상관 없다.
  */
