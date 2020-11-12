eb15fd98d2833d2c2b032b595037944edda01aa2
1000JI <chjh1992@gmail.com>
2020-11-12 12:02:44 +0900

    feat: basic setting, dummy data

---
+++ Example/season2/step3+Empty/RxSwift+MVVM/Pages/Menu.swift
@@ -0,0 +1,16 @@
+//
+//  Menu.swift
+//  RxSwift+MVVM
+//
+//  Created by 천지운 on 2020/11/12.
+//  Copyright © 2020 iamchiwon. All rights reserved.
+//
+
+import Foundation
+
+// ViewModel
+struct Menu {
+    var name: String
+    var price: Int
+    var count: Int
+}
---
+++ Example/season2/step3+Empty/RxSwift+MVVM/Pages/MenuList/MenuListViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  MenuListViewModel.swift
+//  RxSwift+MVVM
+//
+//  Created by 천지운 on 2020/11/12.
+//  Copyright © 2020 iamchiwon. All rights reserved.
+//
+
+import Foundation
+
+class MenuListViewModel {
+    let menus: [Menu] = [
+        Menu(name: "테스트1", price: 100, count: 0),
+        Menu(name: "테스트2", price: 100, count: 0),
+        Menu(name: "테스트3", price: 100, count: 0),
+        Menu(name: "테스트4", price: 100, count: 0),
+        Menu(name: "테스트5", price: 100, count: 0)
+    ]
+}
